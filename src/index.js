89659f1ce92eb1f9849bf947e30ad340bc692874
trungne <nguyenquochoangtrung@gmail.com>
2021-07-23 23:08:48 +0700

    create initial structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,65 @@
+import ('./styles.css');
+
+addTask = function(taskInputField, taskField){
+    if (taskInputField.value){
+        let confirmed = true;
+        for (let task of taskField.childNodes){
+            if (taskInputField.value === task.textContent){
+                confirmed = confirm(`It seems that ${task.textContent} has already been added. You still want to add it?`);
+            }
+        }
+
+        if (confirmed){
+            const div = document.createElement("div");
+            div.textContent = taskInputField.value;
+            taskField.appendChild(div);
+        }
+    }
+    else{
+        alert("Doing nothing is not quite a task, right?")
+    }
+}
+
 initializeHtmlTags = function(){
-    
+    // header text
+    const title = document.createElement("div");
+    const titleText = document.createElement("h1");
+    title.classList.add("header")
+    titleText.textContent = "To-do List";
+    title.appendChild(titleText);
+
+    // div for adding task
+    const addingTaskDiv = document.createElement("div");
+
+    const form = document.createElement("form");
+    form.onsubmit = () => {return false;}
+
+    // input text for adding task
+    const taskInput = document.createElement("input");
+    taskInput.type = "text";
+    taskInput.placeholder = "Enter your task";
+
+    const addTaskBtn = document.createElement("input");
+    addTaskBtn.type = "submit";
+    addTaskBtn.value = "Add";
+
+    form.appendChild(taskInput);
+    form.appendChild(addTaskBtn);
+
+    const tasks = document.createElement("div");
+    addingTaskDiv.appendChild(form);
+    addingTaskDiv.appendChild(tasks);
+
+    addTaskBtn.addEventListener("click", () => {
+        addTask(taskInput, tasks);
+    });
+
+    // append all tags in body in main tag
+    const main = document.createElement("main");
+    main.appendChild(title);
+    main.appendChild(addingTaskDiv);
+
+    document.body.appendChild(main);    
 }
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 };
