97e2ea56211065b2d22d28e7cc7e39a78081c3dd
trungne <nguyenquochoangtrung@gmail.com>
2021-09-22 18:32:27 +0700

    pagination now at btm

--- src/task.js
+++ src/task.js
@@ -1,5 +1,55 @@
 import * as CLASS from './classnames';
 
+// Check if `child` is a descendant of `parent`
+const isDescendant = function(parent, child) {
+    let node = child.parentNode;
+    while (node) {
+        if (node === parent) {
+            return true;
+        }
+
+        // Traverse up to the parent
+        node = node.parentNode;
+    }
+
+    // Go up until the root but couldn't find the `parent`
+    return false;
+};
+
+
+const editText = function(event){
+    const task = event.target;
+    const parent = task.parentNode;
+
+    const form = document.createElement("input");
+    form.type = "text";
+    form.value = task.textContent;
+    form.classList.add("form-text");
+    parent.replaceChild(form, task);
+
+    form.focus();
+    form.select();
+
+    var removed = false;
+    // edit task when enter is pressed
+    form.addEventListener("keydown", (e) =>{
+        if (e.key === "Enter"){ 
+            task.textContent = e.target.value;
+            form.removeEventListener("focusout", replaceTask);
+            e.target.dispatchEvent(new Event("focusout"));
+        }
+    }); 
+    // remove edit field when lose focus
+    form.addEventListener("focusout", (e) => {
+        replaceTask(e, task);
+    });
+}
+
+const replaceTask = function(e, task){
+    const parentNode = e.target.parentNode;
+    parentNode.replaceChild(task, e.target);
+}
+
 const createTask = function(description, id){
     const taskItem = document.createElement("li");
     taskItem.classList.add("list-group-item");
@@ -13,13 +63,14 @@ const createTask = function(description, id){
     const taskCheck = document.createElement("input");
     taskCheck.classList.add(CLASS.TASK_CHECK, "form-check-input");
     taskCheck.type = "checkbox";
-    taskCheck.setAttribute("value", "");
+    taskCheck.value = "";
     taskCheck.id = id;
 
     const taskLabel = document.createElement("label");
     taskLabel.classList.add(CLASS.TASK_DESCRIPTION, "form-check-label");
     taskLabel.textContent = description;
-    taskLabel.setAttribute("contenteditable", true);
+    taskLabel.addEventListener("click", editText);
+
     // taskLabel.setAttribute("for", taskCheck.id);
 
     const removeBtn = document.createElement("button");
